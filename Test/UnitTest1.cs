7bcaf5e174126b0a6c1446125032bbbd79dbb112
Sunny Zeng <sunny.zeng@outlook.com>
2016-05-25 23:54:43 +1000

    added invalid json request unit test

--- Test/UnitTest1.cs
+++ Test/UnitTest1.cs
@@ -4,6 +4,9 @@ using CodingChallenge.Controllers;
 using CodingChallenge.Models;
 using System.Net;
 using Newtonsoft.Json;
+using System.Net.Http;
+using System.Web.Http.Hosting;
+using System.Web.Http;
 
 namespace Test
 {
@@ -11,7 +14,7 @@ namespace Test
     public class UnitTest1
     {
         [TestMethod]
-        public void TestInvalidJsonRequest()
+        public void TestEmptyJsonRequest()
         {
             ShowsController controller = new ShowsController();
  
@@ -19,6 +22,20 @@ namespace Test
             var response = controller.FilterShows(request);
 
             Assert.AreEqual(response.StatusCode, HttpStatusCode.OK);
+        }
+
+        [TestMethod]
+        public void TestInvalidJson()
+        {
+            ShowsController controller = new ShowsController();
+            controller.Request = new HttpRequestMessage();
+            controller.Request.Properties.Add(HttpPropertyKeys.HttpConfigurationKey, new HttpConfiguration());
+            // request will be null in case of bad json string
+            JsonRequest request = null;
+            //act 
+            var response = controller.FilterShows(request);
+
+            Assert.AreEqual(response.StatusCode, HttpStatusCode.BadRequest);
         }
     }
 }
